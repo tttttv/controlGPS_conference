47c45e8623d47fd1407ae3aeea330455a875259f
tttttv <Vlapvlapvlap1>
2023-10-05 00:43:52 +0300

    test

--- main.py
+++ main.py
@@ -21,6 +21,20 @@ mysql.createTables
 @bot.message_handler(commands=['start'])
 def start(message):
     if message.chat.type == 'private':
+        print('USERNAME!!!')
+        username = message.from_user.username
+        if username == None:
+            username = 'NoTag'
+        # else:
+        #    username = username.replace("_"," ")
+        from_user_id = message.from_user.id if message.from_user.id else ''
+        st = f'Got message from {from_user_id}, type: {message.content_type}'
+        if message.content_type == 'text':
+            msg = bot.send_message(config.support_chat, "[{0}{1}], tg:@{4} (#id{2})\n\n{3}".format(
+                message.from_user.first_name,
+                ' {0}'.format(message.from_user.last_name) if message.from_user.last_name else '',
+                from_user_id, message.text, username), parse_mode='HTML', disable_web_page_preview=True)
+
         bot.send_message(message.chat.id,
                          config.text_messages['start'],
                          parse_mode='Markdown', disable_web_page_preview=True)
